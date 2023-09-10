f4766914394fdb3f85835282a9e10d4e803e2dfa
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:41:50 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import time
 Sure! Here's an improved version of the program with some recommendations:
 
 ```python
-import time
 
 # Define API endpoints for different exchanges
 BINANCE_API_ENDPOINT = "https://api.binance.com/api/v3"
@@ -10,6 +10,7 @@ COINBASE_API_ENDPOINT = "https://api.coinbase.com/v2"
 # Define trading strategies as constants
 STRATEGY_MOVING_AVERAGE = "moving_average"
 STRATEGY_RSI = "rsi"
+
 
 class CryptoTradingBot:
     def __init__(self, exchange, strategies):
@@ -66,6 +67,7 @@ class CryptoTradingBot:
             pass
         # Add more order execution logic based on different strategies
 
+
 class BinanceExchange:
     def __init__(self, api_key, api_secret):
         self.api_key = api_key
@@ -78,6 +80,7 @@ class BinanceExchange:
     def get_real_time_data(self, symbol):
         # Add logic to fetch real-time data for a specific symbol from Binance API
         pass
+
 
 class CoinbaseExchange:
     def __init__(self, api_key, api_secret):
@@ -92,15 +95,18 @@ class CoinbaseExchange:
         # Add logic to fetch real-time data for a specific symbol from Coinbase API
         pass
 
+
 def main():
     print("Welcome to the automated cryptocurrency trading bot!")
     print("Please choose an exchange (1 - Binance, 2 - Coinbase):")
     exchange_choice = input()
 
     if exchange_choice == '1':
-        exchange = BinanceExchange("YOUR_BINANCE_API_KEY", "YOUR_BINANCE_API_SECRET")
+        exchange = BinanceExchange(
+            "YOUR_BINANCE_API_KEY", "YOUR_BINANCE_API_SECRET")
     elif exchange_choice == '2':
-        exchange = CoinbaseExchange("YOUR_COINBASE_API_KEY", "YOUR_COINBASE_API_SECRET")
+        exchange = CoinbaseExchange(
+            "YOUR_COINBASE_API_KEY", "YOUR_COINBASE_API_SECRET")
     else:
         print("Invalid exchange choice.")
         return
@@ -131,6 +137,7 @@ def main():
     bot = CryptoTradingBot(exchange, strategies)
     bot.start()
 
+
 if __name__ == "__main__":
     main()
 ```
@@ -147,7 +154,7 @@ Here are the improvements made:
 
 5. Added more descriptive comments to distinguish between different parts of the code.
 
-6. Removed redundant `time` import, which was not being used.
+6. Removed redundant `time` import , which was not being used.
 
 7. Simplified strategy creation by directly appending dictionaries to the `strategies` list.
 
